0e1788c6c93d2820d7cd225f2f1fcfec7f9089de
kiddnoke <kiddnoke@163.com>
2018-11-04 07:33:50 +0800

    修改PM2配置文件的参数

--- pm2.config.js
+++ pm2.config.js
@@ -1,16 +1,8 @@
 module.exports = {
-  apps : [{
-    name        : "nodemgr",
-    script      : "app.js",
-    cwd : "./NodeMgrAgent/",
-    args:['-H','10.0.2.70', '-P' ,'7001', '-h','10.0.2.72' ,'-R','10000:20000' ,'-M','8001' ,'-C','8002' ,'-S','CN' ,'-A','1' ],
-    watch       : true,
-  },{
-    name       : "sssmanager",
-    script     : "onlymanager.py",
-    cwd : "./VpnEdge/shadowsocks/",
-    args:['--manager-address','127.0.0.1:8001'],
-    interpreter   : "python",
-    watch       : true,
+  apps: [{
+    name: "nodemgr",
+    script: "app.js",
+    args: ['-H', 'localhost', '-S', 'State', '-A', Area],
+    watch: true,
   }]
 };
