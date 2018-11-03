cdce132a58fbbd77cff12b8a8fed3c1d248ca5c0
kiddnoke <kiddnoke@163.com>
2018-11-03 23:18:19 +0800

    添加node 和 git版本识别安装逻辑

--- start.edge.sh
+++ start.edge.sh
@@ -4,25 +4,35 @@ cd ~
 yum install epel-release -y 
 yum install wget curl -y 
 # install nodejs 
-wget https://nodejs.org/dist/v10.13.0/node-v10.13.0-linux-x64.tar.xz
-xz -d node-v10.13.0-linux-x64.tar.xz 
-tar xvf node-v10.13.0-linux-x64.tar
-cd node-v10.13.0-linux-x64
-cp -r * /usr
+cd ~
+node_version=$(node --version)
+if [ $node_version != 'v10.13.0'];then
+    wget https://nodejs.org/dist/v10.13.0/node-v10.13.0-linux-x64.tar.xz
+    xz -d node-v10.13.0-linux-x64.tar.xz 
+    tar xvf node-v10.13.0-linux-x64.tar
+    cd node-v10.13.0-linux-x64
+    cp -r * /usr
+fi
  
 # install git 
-yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils-MakeMaker -y
-wget https://www.kernel.org/pub/software/scm/git/git-2.13.0.tar.gz   
-tar -zxvf git-2.13.0.tar.gz 
-cd git-2.13.0
-./configure prefix=/usr/local/
-make -j8 && make install 
+cd ~
+git_version=$(git --version)
+if [ $git_version == 'git version 2.*'] ;then
+    yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils-MakeMaker -y
+    wget https://www.kernel.org/pub/software/scm/git/git-2.13.0.tar.gz   
+    tar -zxvf git-2.13.0.tar.gz 
+    cd git-2.13.0
+    ./configure prefix=/usr/local/
+    make -j8 && make install 
+fi 
+
+
 # pull VpnEdge
 cd ~
 rm -rf VpnEdge 
 git clone https://github.com/kiddnoke/VpnEdge.git
 cd VpnEdge
-git checkout -b mutilthread origin/mutilthread
+git checkout v1.0.0
 python setup.py install --record install.txt 
 cat install.txt | xargs rm -f 
 python setup.py install --record install.txt 
@@ -31,5 +41,5 @@ python setup.py install --record install.txt
 cd ~ 
 git clone https://github.com/kiddnoke/NodeMgrAgent.git
 cd NodeMgrAgent
-git checkout -b timeoutclear origin/timeoutclear
+git checkout v1.0.0
 npm i 
